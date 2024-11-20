0f846882f2afcad898324aa27e5035490598eae2
Stefan Lupu <stefan.n.lupu@gmail.com>
2024-11-20 01:36:40 +0000

    Add token definitions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module monkeyintp
+
+go 1.23.3
---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	ADD    = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
